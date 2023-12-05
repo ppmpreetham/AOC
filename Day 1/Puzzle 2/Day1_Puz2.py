23b921929ccff66e75db35e8711588b6c2507904
Preetham Pemmasani <ppmpreetham1@gmail.com>
2023-12-05 18:43:36 +0530

    Update Puzzle 2

--- Day 1/Puzzle 2/Day1_Puz2.py
+++ Day 1/Puzzle 2/Day1_Puz2.py
@@ -1,35 +1,25 @@
 with open("Day 1/Puzzle 1/Day1_Puz1.txt") as file:
     contents = file.read()
-
-example="""two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen"""
 summ=0
 alpa={
-    "one":1,
-    "two":2,
-    "three":3,
-    "four":4,
-    "five":5,
-    "six":6,
-    "seven":7,
-    "eight":8,
-    "nine":9
+    "one":"o1ne",
+    "two":"t2wo",
+    "three":"th3re",
+    "four":"fo4ur",
+    "five":"fi5ve",
+    "six":"si6x",
+    "seven":"sev7en",
+    "eight":"ei8ght",
+    "nine":"ni9ne"
 }
-
-for i in example.split("\n"):
+for i in contents.split("\n"):
     fwd,bwd=0,0
     for key, val in alpa.items():
         i = i.replace(key, str(val))
-        print(i)
     for j in i:
         if j.isdigit():
             fwd=j
             if bwd==0:
                 bwd=j
-    summ += int(fwd+bwd)
+    summ += int(bwd+fwd)
 print(summ)
