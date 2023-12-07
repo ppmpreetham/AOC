743b32a352277b1110782946b3ddfb617c4eaeea
Preetham Pemmasani <ppmpreetham1@gmail.com>
2023-12-07 13:12:46 +0530

    Update Day6_Puz_2.py

--- Day 6/Puzzle 2/Day6_Puz_2.py
+++ Day 6/Puzzle 2/Day6_Puz_2.py
@@ -1,9 +1,9 @@
 import re
 with open("Day 6/Puzzle 1/Day6_Puz1.txt") as file:
     contents = file.read()
-ans=0
 time = int(''.join(re.findall(r'\d+', contents.strip().split('\n')[0])).replace(" ", ""))
 distance=int(''.join(re.findall(r'\d+', contents.strip().split('\n')[1])).replace(" ", ""))
+ans=0
 for x in range(time):
     if (time-x)*x>distance:
         ans+=1
