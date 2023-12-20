618c2230d332789a77196c8c3a06465dca5b569a
Preetham Pemmasani <75422607+Preetham-ai@users.noreply.github.com>
2023-12-20 19:12:24 +0530

    Update Day5_Puz1.py

--- Day 5/Puzzle 1/Day5_Puz1.py
+++ Day 5/Puzzle 1/Day5_Puz1.py
@@ -53,6 +53,8 @@ def mapper(para):
             keymap[start] = end
         
         finalmap.update(keymap)
+        #edit: for I in range(end):
+        # if I in set, ignore, else add to set
     return finalmap
 
 def finder(dict1, dict2):
