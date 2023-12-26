9799d01dbc1558c1132651e47b859c014e87a835
Preetham Pemmasani <75422607+Preetham-ai@users.noreply.github.com>
2023-12-26 14:39:51 +0530

    Update Day1_Puz1.py

--- Day 1/Puzzle 1/Day1_Puz1.py
+++ Day 1/Puzzle 1/Day1_Puz1.py
@@ -2,7 +2,7 @@ with open("Day 1/Puzzle 1/Day1_Puz1.txt") as file:
     contents = file.read()
 summ=0
 for i in contents.split("\n"):
-    fwd,bwd=0,0
+    fwd=bwd=0
     for j in i:
         if j.isdigit():
             fwd=j
