68bb6d2a93dcd9c024423f4c14b51fbb3b13fc37
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-12-02 11:47:31 +0530

    change input

--- _2024/day-02/problem 2/problem2.py
+++ _2024/day-02/problem 2/problem2.py
@@ -1,4 +1,4 @@
-data = [[int(n) for n in line.split()] for line in open("c:/Users/ppmpr/OneDrive/Documents/GitHub/AOC/_2024/day-02/problem 1/input.txt").read().splitlines()]
+data = [[int(n) for n in line.split()] for line in open("c:/Users/ppmpr/OneDrive/Documents/GitHub/AOC/_2024/day-02/problem 2/input.txt").read().splitlines()]
 
 def isok(lst):
     diff = [x - y for x, y in zip(lst, lst[1:])]
