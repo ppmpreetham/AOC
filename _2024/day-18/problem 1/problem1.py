9069c24323564d0846fa477410edc0b747801f62
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-12-23 21:09:50 +0530

    inp to map

---
+++ _2024/day-18/problem 1/problem1.py
@@ -0,0 +1,39 @@
+inps = """5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"""
+
+mem = ""
+
+filter_inps = [list(map(int,line.split(','))) for line in inps.split("\n")]
+# print(filter_inps)
+for i in range(7):
+    for j in range(7):
+        if [i,j] in filter_inps:
+            mem += '#'
+        else:
+            mem += '.'
+    mem+= '\n'
+
+print(mem)
